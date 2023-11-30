245857803d9412e21b7a76277df72e74f9a64885
trevor dewindt <trevor.dewindt@gmail.com>
2023-11-30 01:12:24 -0700

    replace anchor with link hook

--- src/pages/RouteError.js
+++ src/pages/RouteError.js
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function RouteError() {
     return (
         <div id="error-page-container">
             <h1>We could not find what you are looking for.</h1>
-            <h3>Go back to the <a href="http://localhost:3001/">homepage</a></h3>
+            <h3>Go back to the <Link to="/">homepage</Link></h3>
         </div>
     )
 }
